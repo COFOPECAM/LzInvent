66698eecdc9b590a6f56a082c395720fc1508dfa
Alexis Fuentes <alexis.fuentes@cofopecam.org.mx>
2019-08-20 09:30:39 -0500

    Validación y funciones extra en el formulario de agregar usuario

--- forms/u_frmadduser.pas
+++ forms/u_frmadduser.pas
@@ -6,17 +6,19 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, DBCtrls,
-  ButtonPanel, ZDataset, m_conn, db, BCrypt, LCLType;
+  ButtonPanel, ZDataset, m_conn, db, BCrypt, LCLType, Buttons;
 
 type
 
   { TFrmUser }
 
   TFrmUser = class(TForm)
+    BtnViewPass: TBitBtn;
     ButtonPanel1: TButtonPanel;
     DSRoles: TDataSource;
     Datos: TGroupBox;
     CbRoles: TDBLookupComboBox;
+    ILimgs: TImageList;
     TxtNombres: TEdit;
     TxtApellidos: TEdit;
     TxtUsuario: TEdit;
@@ -27,6 +29,7 @@ type
     Label4: TLabel;
     Label5: TLabel;
     ZQUser: TZQuery;
+    procedure BtnViewPassClick(Sender: TObject);
     procedure CancelButtonClick(Sender: TObject);
     procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
     procedure FormShow(Sender: TObject);
@@ -35,6 +38,9 @@ type
     save: Boolean;
 
   public
+    edit: Boolean;
+    user_id: integer;
+    view_pass: Boolean;
 
   end;
 
@@ -81,16 +87,56 @@ end;
 procedure TFrmUser.FormShow(Sender: TObject);
 begin
   save:=true;
+  if edit then
+  begin
+    ZQUser.SQL.Text:='select u.nombres, u.apellidos, u.usuario, u.roles_id_roles'+
+    ' as role_id from usuarios u where id_usuarios = :id';
+    ZQUser.Params.ParamByName('id').AsInteger:=user_id;
+    ZQUser.Open;
+    TxtNombres.Text:=ZQUser.FieldByName('nombres').Text;
+    TxtApellidos.Text:=ZQUser.FieldByName('apellidos').Text;
+    TxtUsuario.Text:=ZQUser.FieldByName('usuario').Text;
+    CbRoles.KeyValue:=StrToInt(ZQUser.FieldByName('role_id').Text);
+  end;
+  view_pass:=false;
+  BtnViewPass.ImageIndex:=0;
 end;
 
 procedure TFrmUser.FormCloseQuery(Sender: TObject; var CanClose: boolean);
+var
+  i:integer;
 begin
-   CanClose:=save;
+  for i := 0 to ComponentCount-1 do
+  begin
+    if (Components[i] is TEdit) then
+    begin
+      TEdit(Components[i]).Text:='';
+    end;
+  end;
+  CbRoles.KeyValue:=0;
+  edit:=false;
+  CanClose:=save;
 end;
 
 procedure TFrmUser.CancelButtonClick(Sender: TObject);
 begin
   save:=true;
+end;
+
+procedure TFrmUser.BtnViewPassClick(Sender: TObject);
+begin
+  if not view_pass then
+  begin
+    BtnViewPass.ImageIndex:=1;
+    TxtContrasena.EchoMode:=emNormal;
+    view_pass:=true;
+  end
+  else
+  begin
+    view_pass:=false;
+    TxtContrasena.EchoMode:=emPassword;
+    BtnViewPass.ImageIndex:=0;
+  end;
 end;
 
 end.
